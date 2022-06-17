bb5b072987c3bb22d705380a188e72cacfb8001f
LAPTOP-Colocargo <csaysset@e.email>
2022-06-17 13:21:12 +0200

    Add resumePage + Navigation with hamburger menu (SearchItineraryActivity => SearchItineraryFRAGMENT (work with search function) + PadamDailyActivity (new main activity)

--- app/src/main/java/io/padam_exercise/padamdaily/screens/SearchItineraryFragment.kt
+++ app/src/main/java/io/padam_exercise/padamdaily/screens/SearchItineraryFragment.kt
@@ -1,31 +1,41 @@
 package io.padam_exercise.padamdaily.screens
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import io.padam_exercise.padamdaily.models.MarkerType
 import io.padam_exercise.padamdaily.models.mocking.MockSuggestion
 import io.padam_exercise.padamdaily.models.Suggestion
 import io.padam_exercise.padamdaily.utils.Toolbox
-import kotlinx.android.synthetic.main.activity_search_itinerary.*
 import padam_exercise.padamdaily.R
+import padam_exercise.padamdaily.databinding.FragmentSearchItineraryBinding
 
-class SearchItineraryActivity : AppCompatActivity() {
+class SearchItineraryFragment : Fragment() {
 
     private var mMapDelegate: MapActionsDelegate? = null
+    private var _binding: FragmentSearchItineraryBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_itinerary)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchItineraryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
         initMapFragment()
         initView()
+        return root
     }
 
     private fun initMapFragment() {
         val mapFragment: MapFragment = MapFragment.newInstance()
 
-        supportFragmentManager.beginTransaction()
+        requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.cl_map, mapFragment)
             .commitAllowingStateLoss()
 
@@ -44,22 +54,22 @@ class SearchItineraryActivity : AppCompatActivity() {
 
     private fun initDepartureSpinner() {
         val departuresList = Toolbox.getStringListFromSuggestion(MockSuggestion.departures())
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, departuresList)
-        spinner_departure.adapter = adapter
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, departuresList)
+        binding.spinnerDeparture.adapter = adapter
     }
 
     private fun initArrivalSpinner() {
         val arrivalsList = Toolbox.getStringListFromSuggestion(MockSuggestion.arrivals())
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrivalsList)
-        spinner_arrival.adapter = adapter
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, arrivalsList)
+        binding.spinnerArrival.adapter = adapter
     }
 
     private fun manageOnClickSearchItinerary() {
-        btn_search_itinerary.setOnClickListener {
+        binding.btnSearchItinerary.setOnClickListener {
             mMapDelegate?.clearMap()
 
-            val selectedDeparture: String = spinner_departure.selectedItem.toString()
-            val selectedArrival: String = spinner_arrival.selectedItem.toString()
+            val selectedDeparture: String = binding.spinnerDeparture.selectedItem.toString()
+            val selectedArrival: String = binding.spinnerArrival.selectedItem.toString()
 
             val suggestionDeparture = getSuggestionFromSelection(selectedDeparture, MockSuggestion.departures())
             val suggestionArrival = getSuggestionFromSelection(selectedArrival, MockSuggestion.arrivals())
